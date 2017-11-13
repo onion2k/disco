2203fd895657482e0ab981a59ae15440c4defdc2
Chris <chris@usablehq.com>
2017-11-13 20:19:11 +0000

    Ducks with hats

--- src/disco.js
+++ src/disco.js
@@ -33,7 +33,7 @@ function init() {
     plane.rotation.set(-Math.PI/2, 0, 0);
     scene.add( plane );
     
-    let amblight = new AmbientLight( 0x444444 );
+    let amblight = new AmbientLight( 0xaaaaaa );
     scene.add( amblight );
 
     var light = new SpotLight( 0xff4444, 0.75, 0, Math.PI/16, 0.05 );
@@ -82,7 +82,7 @@ function init() {
 
 
 
-    camera = new PerspectiveCamera( 30, 800/600, 1, 5000 );
+    camera = new PerspectiveCamera( 70, 800/600, 1, 5000 );
     camera.position.x = 0;
     camera.position.y = 30;
     camera.position.z = 30;
@@ -102,17 +102,21 @@ function init() {
 
 function animate() {
     c += 1;
-    spots.forEach((spot,i) => {
-        spot.target.position.x += Math.sin(c/100) * 0.1 * spot.motion.x;
-        spot.target.position.z += Math.cos(c/100) * 0.1 *  spot.motion.y;
-    });
+    // spots.forEach((spot,i) => {
+    //     spot.target.position.x += Math.sin(c/100) * 0.1 * spot.motion.x;
+    //     spot.target.position.z += Math.cos(c/100) * 0.1 *  spot.motion.y;
+    // });
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
 }
 
 const models = [
-    { id: 'bill', path: 'duck/', model: 'RubberDuck.obj', material: 'RubberDuck.mtl', scale: 1, rotation: {x:0, y:0, z:0} },
-    { id: 'bob', path: 'duck/', model: 'RubberDuck.obj', material: 'RubberDuck2.mtl', scale: 1, rotation: {x:0, y:0, z:0} }
+    { id: 'duck', path: 'duck/', model: 'RubberDuck.obj', material: 'RubberDuck.mtl', scale: 1, rotation: {x:0, y:0, z:0} },
+    { id: 'duck2', path: 'duck/', model: 'RubberDuck.obj', material: 'RubberDuck2.mtl', scale: 1, rotation: {x:0, y:0, z:0} },
+    { id: 'wizard', path: 'hats/wizard/', model: 'wizard hat.obj', material: 'wizard hat.mtl', scale: 1, rotation: {x:0, y:0, z:0} },
+    { id: 'pirate', path: 'hats/pirate/', model: 'pirate hat.obj', material: 'pirate hat.mtl', scale: 1, rotation: {x:0, y:0, z:0} },
+    { id: 'top', path: 'hats/tophat/', model: 'top hat.obj', material: 'top hat.mtl', scale: 1, rotation: {x:0, y:0, z:0} },
+    { id: 'sheriff', path: 'hats/sheriff/', model: 'SheriffHat.obj', material: 'SheriffHat.mtl', scale: 1, rotation: {x:0, y:0, z:0} }
 ]
 
 pl.load(models).then((result) => {
@@ -123,20 +127,67 @@ pl.load(models).then((result) => {
     init();
     animate();
 
-    modelsArr.bill.position.x -= 5;
-    modelsArr.bob.position.x += 5;
 
-    scene.add(modelsArr.bill);
-    scene.add(modelsArr.bob);
+
+    let barry = modelsArr.duck.clone();
+    barry.position.x -= 15;    
+    scene.add(barry);
+    
+    let th = modelsArr.top.clone();
+    barry.add(th);
+    th.position.set(0,6.1,1.25);
+
+    let t3 = new TimelineMax({ repeat: -1 });
+    t3.add(TweenLite.to(barry.rotation, 1, { y: Math.PI * 2 }));
+    t3.add(TweenLite.to(barry.position, 0.5, { y: 5, ease: Power2.easeOut }));
+    t3.add(TweenLite.to(barry.position, 0.5, { y: 0, ease: Power2.easeIn }));
+
+
+
+    let bill = modelsArr.duck.clone();
+    bill.position.x -= 5;
+    scene.add(bill);
+
+    let wz = modelsArr.wizard.clone();
+    bill.add(wz);
+    wz.position.set(0,4.45,1.25);
 
     let t1 = new TimelineMax({ repeat: -1 });
-    t1.add(TweenLite.to(modelsArr.bill.rotation, 1, { y: Math.PI*2 }));
-    t1.add(TweenLite.to(modelsArr.bill.position, 0.5, { y: 5, ease: Power2.easeOut }));
-    t1.add(TweenLite.to(modelsArr.bill.position, 0.5, { y: 0, ease: Power2.easeIn }));
+    t1.add(TweenLite.to(bill.rotation, 1, { y: Math.PI * -2 }));
+    t1.add(TweenLite.to(bill.position, 0.5, { y: 5, ease: Power2.easeOut }));
+    t1.add(TweenLite.to(bill.position, 0.5, { y: 0, ease: Power2.easeIn }));
+
+
+
+    let bob = modelsArr.duck.clone();
+    bob.position.x += 5;
+    scene.add(bob);
+
+    let ph = modelsArr.pirate.clone();
+    bob.add(ph);
+    ph.position.set(0,4.9,1.25);
 
     let t2 = new TimelineMax({ repeat: -1 });
-    t2.add(TweenLite.to(modelsArr.bob.rotation, 1, { y: Math.PI * -2 }));
-    t2.add(TweenLite.to(modelsArr.bob.position, 0.5, { y: 5, ease: Power2.easeOut }));
-    t2.add(TweenLite.to(modelsArr.bob.position, 0.5, { y: 0, ease: Power2.easeIn }));
+    t2.add(TweenLite.to(bob.rotation, 1, { y: Math.PI * 2 }));
+    t2.add(TweenLite.to(bob.position, 0.5, { y: 5, ease: Power2.easeOut }));
+    t2.add(TweenLite.to(bob.position, 0.5, { y: 0, ease: Power2.easeIn }));
+
+
+
+
+    let benny = modelsArr.duck.clone();
+    benny.position.x += 15;    
+    scene.add(benny);
     
+    let sh = modelsArr.sheriff.clone();
+    benny.add(sh);
+    sh.position.set(0,5.2,1.25);
+
+    let t4 = new TimelineMax({ repeat: -1 });
+    t4.add(TweenLite.to(benny.rotation, 1, { y: Math.PI * -2 }));
+    t4.add(TweenLite.to(benny.position, 0.5, { y: 5, ease: Power2.easeOut }));
+    t4.add(TweenLite.to(benny.position, 0.5, { y: 0, ease: Power2.easeIn }));
+
+
+
 });
